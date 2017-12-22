786acf07cb70fc9f6277db2ff7dc7a65dd9c0285
Anton Morev <shikto@mail.ru>
2017-12-22 14:47:41 +0300

    wip

--- exceptions/WrongAppIdMobileSolutionsAuthException.php
+++ exceptions/WrongAppIdMobileSolutionsAuthException.php
@@ -8,7 +8,14 @@
 
 namespace Zvinger\Auth\Mobsolutions\exceptions;
 
+use Throwable;
+
 class WrongAppIdMobileSolutionsAuthException extends BaseMobileSolutionsAuthException
 {
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = NULL)
+    {
+        $message = "This App Id is wrong";
+        parent::__construct($message, $code, $previous);
+    }
 
 }
